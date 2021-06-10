beefe5a2d2a767177b667402579c8b8f37e8723a
hectorvejar <85649615+hectorvejar@users.noreply.github.com>
2021-06-10 16:04:59 -0600

    Add files via upload

--- server/src/schema.js
+++ server/src/schema.js
@@ -7,30 +7,38 @@ const typeDefs = gql `
      id:ID!
      correo:String
      pass:String
-     listaDeseados:[ID]
  }
-input listaInput{
-    idProducto:ID
-}
  type Producto{
      id:ID!
      nombre:String
      descripcion:String
      precio:Float
-     imagen:String!
+     imagen:[String]
+     sitioWeb:[String]
+     url:[String]
+ }
+ type ListaDeseados{
+     id:ID
+     productos:[Producto]
  }
 
 type Query{
-   getListaDeseados(id:ID!):Usuario
+   getListaDeseados(id:ID!):ListaDeseados
+   
    getPrecioProducto(id:ID!):Producto
    getInfoUsuario(id:ID):Usuario
  }
 
  type Mutation{
      updatePrecioProducto(productoID:ID!,nuevoPrecio:Float!): Producto                                                                          
+    
+     agregarLista(idUsuario:ID!,producto:Producto):ListaDeseados
+     
      registroUsuarioNuevo(correo:String!,pass:String): Usuario
-     agregarLista(idUsuario:ID!,idProducto:ID!):Usuario
- }
+     eliminarCuenta(id:ID!):Usuario
+     cambiarCorreo(id:ID!,nuevoCorreo:String!):Usuario
+     cambiarPass(id:ID!,nuevaPass:String!):Usuario
+ }  
  `;
      //Mutation:modifica datos en la base de datos y los retorna lo modificado/eliminado/insertado
      //sintaxis: nombre(loQueOcupe:Tipo):LoQueRegresa , eso ultimo que regresa suele ser algo ya definido en el schema
